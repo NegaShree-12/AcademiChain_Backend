40fd4e29dcdac5c5cd8a763ab3e7c3e6a9af9316
NEGASHREE <negashree2203@gmail.com>
2026-03-19 15:08:51 +0530

    Add new preview URL to CORS

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ const allowedOrigins = [
   'https://c0d4e85db19593.lhr.life',
   'https://academicchain-frontend.vercel.app',
   'https://academichain-frontend.vercel.app',
+  'https://academichain-frontend-ozlnoazea-negashrees-projects.vercel.app',
   'https://academichain-frontend-ntx0qswdr-negashrees-projects.vercel.app', // ADD THIS
   process.env.FRONTEND_URL,
   process.env.FRONTEND_PUBLIC_URL
