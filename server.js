4e6e9b820ba3d90c2805406861ea20e436b20011
NEGASHREE <negashree2203@gmail.com>
2026-03-19 14:49:21 +0530

    bug cleared

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ const allowedOrigins = [
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
   'https://c0d4e85db19593.lhr.life',
-  'https://academichain-frontend.vercel.app',  // ← ADD THIS LINE
+  'https://academicchain-frontend.vercel.app',
+  'https://academichain-frontend.vercel.app',
+  'https://academichain-frontend-ozlnoazea-negashrees-projects.vercel.app', // ADD THIS
   process.env.FRONTEND_URL,
   process.env.FRONTEND_PUBLIC_URL
 ].filter(Boolean);
